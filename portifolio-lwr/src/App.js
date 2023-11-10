7ada7af6d76ef4591ecbea9d7b3a279ca4cb496b
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2023-11-10 19:23:14 -0300

    Creating with react

---
+++ portifolio-lwr/src/App.js
@@ -0,0 +1,19 @@
+import './css/styles.css';
+
+function App() {
+  return (
+    <body>
+      <section id="person">
+        <div class="person-content">
+          <div class="person-name">
+            <h1>Olá</h1>
+            <h1>Meu nome é</h1>
+            <h1>Lucas Witt da Rosa</h1>
+          </div>
+        </div>
+      </section>
+    </body>
+  );
+}
+
+export default App;
